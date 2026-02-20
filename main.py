082c413ea29f663c762dd333d18fee6c2729b557
Jayden-code-art <yin.ja@wcs-g.com>
2026-02-20 15:32:21 -0700

    Update pxt.json, main.blocks, main.ts, main.py, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+mySprite = sprites.create(assets.image("""
+    cube1
+    """), SpriteKind.player)
+mySprite.ay = 600
+mySprite.vx = 120
+mySprite.set_position(20, 80)
+scene.camera_follow_sprite(mySprite)
+for index in range(31):
+    pass
--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+let mySprite = sprites.create(assets.image`cube1`, SpriteKind.Player)
+mySprite.ay = 600
+mySprite.vx = 120
+mySprite.setPosition(20, 80)
+scene.cameraFollowSprite(mySprite)
+for (let index = 0; index <= 30; index++) {
+	
+}
